660de3b192c85886506b3ca8351b2f7ab70f38e2
ilmir <adm.eths@gmail.com>
2025-02-17 21:05:12 +0500

    add code in main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-asdas
+import random
+try:
+    random_num = random.randrange(1,100)
+    user_answer = int(input("Введите число: "))
+    attempt_amount = 1 # Число попыток
+
+    while attempt_amount <= 10:
+        if user_answer == random_num:
+            print(f'Вы угадали, загаданное число: {random_num}, за {attempt_amount} попыток')
+            break
+        elif user_answer > random_num:
+            print('Ваше число больше')
+            attempt_amount += 1
+            user_answer = int(input("Напишите число: "))
+        elif user_answer < random_num:
+            print('Ваше число меньше')
+            attempt_amount += 1
+            user_answer = int(input("Напишите число: "))
+        else:
+            print("Кол-во попыток исчерпано")
+except ValueError:
+    print("Недопустимое значение")
+
+
+
