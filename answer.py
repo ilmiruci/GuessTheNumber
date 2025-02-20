df846edbd6c7e4be566e79a25306208eb681747d
ilmir <adm.eths@gmail.com>
2025-02-20 14:14:30 +0500

    add get_user_num add text.py

---
+++ answer.py
@@ -0,0 +1,18 @@
+def get_user_num() -> int:
+        num: int = int(input("Введите число: "))
+
+        return num
+
+        # if num <= 100 and num >= 0:
+        #     return num
+        # elif num < 1 or num > 100:
+        #     print("Число должно быть в диапазоне от 1 до 100")
+        #     num: int = int(input("Введите число: "))
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+import random, answer, text
 
 
 # TODO: Переписать код в функциональном стиле.
@@ -14,11 +14,15 @@ def game_logic():
     random_num = random.randrange(1, 100)
     attempt_amount = 0  # Число попыток
 
+    print(text.game_name, text.sep, text.rules, text.sep)
+
+
     while attempt_amount < 3:
         attempt_amount += 1
         try:
-            user_answer = int(input("Введите число: "))
+            user_answer = answer.get_user_num()
             # TODO: Что если, пользователь ввел число вне диапазона?
+            # Вынес запрос ответа пользователя (с проверкой входного значения) в отдельный модуль
 
             if user_answer == random_num:
                 print(f'Вы угадали, загаданное число: {random_num}, за {attempt_amount} попыток')
@@ -29,11 +33,14 @@ def game_logic():
             elif user_answer < random_num:
                 print('Ваше число меньше')
         except ValueError:
-            print("Недопустимое значение")
+            print("Неправильное значение")
 
     else:
         # TODO: Вывести информацию какое число было загадано.
+        # Вывел
+        print(text.sep)
         print("Кол-во попыток исчерпано")
+        print(f"Загаданное число {random_num}")
 
 
 def run():
